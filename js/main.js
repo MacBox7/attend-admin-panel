52439f7eeadd6a106f9d3018cb7e377fef6921ba
MacBox7 <ajankit2304@gmail.com>
2017-10-06 01:15:02 +0530

    Event listeners for capture and register

--- js/main.js
+++ js/main.js
@@ -1,41 +1,89 @@
- $(document).ready(function () {
-     materializeInit();
- });
+(function ($) {
 
- $("#anchor-preprocess-images, #mobile-anchor-preprocess-images").click(function () {
-     swal({
-         title: "Are you sure?",
-         text: "This will process the complete database!",
-         type: "warning",
-         showCancelButton: true,
-         confirmButtonColor: "#DD6B55",
-         confirmButtonText: "Yes, process it!",
-         cancelButtonText: "No, cancel plx!",
-         closeOnConfirm: false,
-         closeOnCancel: false
-     }).then(function () {
-         swal(
-             'Success!',
-             'All images have been processed',
-             'success'
-         )
-     }, function (dismiss) {
-         if (dismiss === 'cancel') {
-             swal(
-                 'Cancelled',
-                 'No processing took place :)',
-                 'error'
-             )
-         }
-     });
- });
+    $.URL = {
+        FORMREGISTER: "something",
+        REFRESHDATABASE: "something"
+    };
 
- $("#mobile-anchor-preprocess-images, #mobile-anchor-register").click(function () {
-     $(".button-collapse").sideNav('hide');
- });
+    $(document).ready(function () {
+        materializeInit();
+    });
 
- function materializeInit() {
-     $(".button-collapse").sideNav();
-     $('select').material_select();
-     $('.modal').modal();
- }
+    $("#anchor-preprocess-images, #mobile-anchor-preprocess-images").click(function () {
+        swal({
+            title: "Are you sure?",
+            text: "This will process the complete database!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, process it!",
+            cancelButtonText: "No, cancel plx!",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        }).then(function () {
+            $.ajax({
+                type: "get",
+                url: URL.REFRESHDATABASE,
+                success: function (data) {}
+            }).done(function (data) {
+                swal(
+                    'Success!',
+                    'All images have been processed',
+                    'success'
+                )
+            }).error(function (data) {
+                swal("Oops", "We couldn't connect to the server!", "error");
+            });
+        }, function (dismiss) {
+            if (dismiss === 'cancel') {
+                swal(
+                    'Cancelled',
+                    'No processing took place :)',
+                    'error'
+                )
+            }
+        });
+    });
+
+    $("#mobile-anchor-preprocess-images, #mobile-anchor-register").click(function () {
+        $(".button-collapse").sideNav('hide');
+    });
+
+    $("#form-register").submit(function (event) {
+
+        localStorage.setItem("formDetails", $("#form-register").serialize());
+        $("#modal-facecapture").modal("open");
+        event.preventDefault();
+
+    });
+
+    $("#startbutton").click(function () {
+        window.takepicture(function () {
+            $.ajax({
+                type: "POST",
+                url: $.URL.FORMREGISTER,
+                data: localStorage.getItem('formDetails'),
+                success: function (data) {
+                    localStorage.removeItem("formDetails");
+                    $("#modal-facecapture").modal("close");
+                    $("#modal-registration-form").modal("close");
+                    alert(data);
+                }
+            });
+        });
+    });
+
+    function materializeInit() {
+        $(".button-collapse").sideNav();
+        $("select").material_select();
+        $("select[required]").css({
+            position: "absolute",
+            display: "inline",
+            height: 0,
+            padding: 0,
+            width: 0
+        });
+        $(".modal").modal();
+    }
+
+})(jQuery);
